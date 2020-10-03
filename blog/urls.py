9b6bf55a2101a110d625855da6b5f71ad897c471
Tim Tezgel <ttezgel@aimediagroup.com>
2020-10-03 12:20:42 -0400

    Add feature to create new blog post

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+
+from .forms import PostForm
 from .models import Post
 
 
@@ -16,3 +18,17 @@ def post_detail(request, pk):
     context = {'post': post}
     return render(request, 'blog/post_detail.html', context)
 
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+        context = {'form': form}
+        return render(request, 'blog/post_edit.html', context)
