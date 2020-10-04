06b91ceaf5a0803be26cf8e23d269b3b3038011d
Tim Tezgel <ttezgel@aimediagroup.com>
2020-10-03 21:29:27 -0400

    Add feature to make drfats and publish them

--- blog/views.py
+++ blog/views.py
@@ -25,7 +25,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -42,10 +41,21 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
         context = {'form': form}
     return render(request, 'blog/post_edit.html', context)
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+    context = {'posts': posts}
+    return render(request, 'blog/post_draft_list.html', context)
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
