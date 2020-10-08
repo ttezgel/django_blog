2834f37042fe5f6a6aa7a34f3461659b79d30927
Tim Tezgel <ttezgel@aimediagroup.com>
2020-10-07 23:59:09 -0400

    Add feature to comment on blog post

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('drafts/', views.post_draft_list, name='post_draft_list'),
     path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
 ]
